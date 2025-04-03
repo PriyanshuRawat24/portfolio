03713668849463ba2840371062978e78f3168a3e
PriyanshuRawat24 <rawatpriyanshu653@gmail.com>
2025-04-03 11:35:49 +0530

    Adjust hero section layout margin for improved spacing

--- src/app/Components/hero.jsx
+++ src/app/Components/hero.jsx
@@ -102,7 +102,7 @@ const Hero = () => {
 
     return (
         <div id='home' className="min-h-screen bg-gray-900 text-white px-4 sm:px-6 md:px-8 lg:px-16 py-16 md:py-24">
-            <main className="max-w-7xl mx-auto flex flex-col-reverse md:flex-row items-center justify-between gap-12 md:gap-8 lg:mt-[200px]">
+            <main className="max-w-7xl mx-auto flex flex-col-reverse md:flex-row items-center justify-between gap-12 md:gap-8 lg:mt-[150px]">
                 <motion.div
                     ref={ref}
                     className="w-full md:w-1/2 space-y-6"
